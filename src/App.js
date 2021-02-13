df566f196da97a8c310112167bb1b0c82ef45b66
hiba <hibaabdelkarim2000@gmail.com>
2021-02-13 21:59:59 +0200

    fix : removed the App.js content

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import 'antd/dist/antd.css';
-import CityItem from './Components/CityItem/CityItem';
 
 function App() {
-  const info =
-    'this is the best thing you will ever have in your life baby if you thought about it this is really cool huh?';
-  return (
-    <div className="App">
-      <CityItem name="saida" info={info} />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
