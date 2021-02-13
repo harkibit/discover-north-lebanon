536ba7a257e67556019c495d57def790d40051ca
hiba <hibaabdelkarim2000@gmail.com>
2021-02-13 21:55:33 +0200

    refactor : fixed the city list item added the see more option as a link in case we have 20+ words

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
 import 'antd/dist/antd.css';
+import CityItem from './Components/CityItem/CityItem';
 
 function App() {
+  const info =
+    'this is the best thing you will ever have in your life baby if you thought about it this is really cool huh?';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CityItem name="saida" info={info} />
     </div>
   );
 }
--- src/Components/CityItem/CityItem.js
+++ src/Components/CityItem/CityItem.js
@@ -1,34 +1,42 @@
 import React from 'react';
+import './cityItem.css';
 import { Card, Image, Row, Col, Typography } from 'antd';
 import { FireOutlined, DoubleRightOutlined } from '@ant-design/icons';
 const { Title } = Typography;
 
-export default function CityItem({
-  imgsrc,
-  cityName,
-  cityInfo,
-  activityNumber,
-}) {
+export default function CityItem(city) {
   return (
     <div>
-      <Card style={{ height: 200, width: 600 }}>
+      <Card className="card-size">
         <Row>
           <Col span={10}>
-            <Image src={imgsrc} width={200} height={150} />
+            <Image src={city.imgsrc} width={200} height={150} />
           </Col>
 
           <Col span={14}>
             <Row>
-              <Title level={3}>{cityName}</Title>
+              <Title level={3}>{city.name}</Title>
             </Row>
 
-            <Row style={{ height: 70 }}>{cityInfo}</Row>
+            <Row className="row-height">
+              {city.info.split(' ').length < 20 ? (
+                city.info
+              ) : (
+                <span>
+                  {' '}
+                  {city.info.split(' ').slice(0, 20).join(' ')}{' '}
+                  <a href="#" className="seeMore">
+                    See more
+                  </a>
+                </span>
+              )}
+            </Row>
 
             <hr />
 
             <Row justify="space-between">
               <span>
-                <FireOutlined /> {activityNumber} Activity
+                <FireOutlined /> {city.activityNumber} Activity
               </span>
               <span>
                 learn more <DoubleRightOutlined />
