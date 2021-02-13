6adcdf08052d7d1ad1e9a9526de9351af242c84d
charbel-t <charbel2000.52@gmail.com>
2021-02-13 15:58:31 +0200

    Feat: add Footer-component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import 'antd/dist/antd.css';
-import ButtonComponont from './Components/./Button/Button';
+import ButtonComponent from './Components/./Button/Button';
+import FooterComponent from './Components/Footer/Footer';
 
 function App() {
   return <div className="App"></div>;
