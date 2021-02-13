a91d19f12829ed53026682d56dbbee28d3dd985a
Bakhos Ibrahim-Rachwan <bakhos.rachwan@gmail.com>
2021-02-13 13:45:45 +0200

    Refactor: remove inline styling

--- src/Components/Card/CityCard/CityCard.js
+++ src/Components/Card/CityCard/CityCard.js
@@ -1,23 +1,15 @@
+import './CityCard.css';
 import { Card } from 'antd';
 const { Meta } = Card;
 
-const CityCard = ({ cityName, imgSrc }) => {
+const CityCard = ({ city }) => {
   return (
     <Card
+      className="card"
       hoverable
-      style={{
-        width: '380px',
-        height: '417px',
-        margin: 'auto',
-        border: '1px solid rgba(0, 145, 234, 0.3)',
-        borderRadius: '5px',
-      }}
-      cover={<img alt={cityName} src={imgSrc} width="375px" height="351px" />}
+      cover={<img alt={city.name} src={city.imgSrc} />}
     >
-      <Meta
-        title={cityName}
-        style={{ textAlign: 'center', fontSize: '18px' }}
-      />
+      <Meta className="meta" title={city.name} />
     </Card>
   );
 };
