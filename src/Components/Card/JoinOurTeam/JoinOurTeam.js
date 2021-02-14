4d748c18fb4ea22ffa638613acdeaea52c692974
Bakhos Ibrahim-Rachwan <bakhos.rachwan@gmail.com>
2021-02-14 11:59:59 +0200

    Feat: join our team card

---
+++ src/Components/Card/JoinOurTeam/JoinOurTeam.js
@@ -0,0 +1,25 @@
+import { Card } from 'antd';
+import ButtonComponont from '../../Button/Button';
+import './JoinOurTeam.css';
+
+const JoinOurTeam = ({ guide }) => {
+  return (
+    <Card className="card">
+      <div className="cardbody">
+        <img alt={guide.name} src={guide.imgSrc} />
+        <div className="info">
+          <h1 className="title">
+            {guide.name} our {guide.job}
+          </h1>
+          <p className="description">{guide.desc}</p>
+          <ButtonComponont type="primary" text="Join Our Team" />
+          <p className="cardfooter">
+            Check the rest of the team <a>Go Here</a>
+          </p>
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+export default JoinOurTeam;
