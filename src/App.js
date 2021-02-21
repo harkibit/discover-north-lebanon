af176f1e67472a3802a773dd36a59aecd78a7ade
AbdelRaouf <123raoufdannaoui@gmail.com>
2021-02-21 20:29:04 +0200

    Feat: add what we do component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import 'antd/dist/antd.css';
-import Navbar from './Components/Navbar/Navbar';
 import React, { Suspense } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <Suspense fallback="loading">
-        <Navbar />
-      </Suspense>
+      <Suspense fallback="loading"></Suspense>
     </div>
   );
 }
