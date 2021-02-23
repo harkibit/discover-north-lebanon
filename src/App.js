282e23e9b7aca6ad715c16547e244a1774d7eaad
AbdelRaouf <123raoufdannaoui@gmail.com>
2021-02-23 17:50:16 +0200

    Fix: return app.js to master state

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
 import 'antd/dist/antd.css';
-import './App.css';
-import DropdownButton from './Components/Dropdown/Dropdown.js';
+import Navbar from './Components/Navbar/Navbar';
+import React, { Suspense } from 'react';
+
 function App() {
-  const citiesArray = [
-    { name: 'city1', id: 0, activities: 'act1' },
-    { name: 'city2', id: 1, activities: 'act3' },
-    { name: 'city3', id: 2, activities: 'act6' },
-  ];
-  const activitiesArray = [
-    { name: 'act1', id: 0, city: 'city1', price: '<100' },
-    { name: 'act2', id: 1, city: 'city1', price: '<200' },
-    { name: 'act3', id: 2, city: 'city2', price: '<300' },
-    { name: 'act4', id: 3, city: 'city3', price: '<100' },
-  ];
-  function handleSelect(cities) {
-    console.log('result :', cities);
-  }
   return (
     <div className="App">
-      <DropdownButton
-        activitiesArray={activitiesArray}
-        elementArray={activitiesArray}
-        handleSelect={handleSelect}
-        elem={'name'}
-        dropdownTitle={'act'}
-      />
+      <Suspense fallback="loading">
+        <Navbar />
+      </Suspense>
     </div>
   );
 }
+
 export default App;
