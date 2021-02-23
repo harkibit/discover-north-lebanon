fe21650976e6e112aa8f947813a0f10883560c46
AbdelRaouf <123raoufdannaoui@gmail.com>
2021-02-23 17:39:20 +0200

    Fix: fix the onClick function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import 'antd/dist/antd.css';
-import Navbar from './Components/Navbar/Navbar';
-import React, { Suspense } from 'react';
-
+import './App.css';
+import DropdownButton from './Components/Dropdown/Dropdown.js';
 function App() {
+  const citiesArray = [
+    { name: 'city1', id: 0, activities: 'act1' },
+    { name: 'city2', id: 1, activities: 'act3' },
+    { name: 'city3', id: 2, activities: 'act6' },
+  ];
+  const activitiesArray = [
+    { name: 'act1', id: 0, city: 'city1', price: '<100' },
+    { name: 'act2', id: 1, city: 'city1', price: '<200' },
+    { name: 'act3', id: 2, city: 'city2', price: '<300' },
+    { name: 'act4', id: 3, city: 'city3', price: '<100' },
+  ];
+  function handleSelect(cities) {
+    console.log('result :', cities);
+  }
   return (
     <div className="App">
-      <Suspense fallback="loading">
-        <Navbar />
-      </Suspense>
+      <DropdownButton
+        activitiesArray={activitiesArray}
+        elementArray={activitiesArray}
+        handleSelect={handleSelect}
+        elem={'name'}
+        dropdownTitle={'act'}
+      />
     </div>
   );
 }
-
 export default App;
--- src/Components/Dropdown/Dropdown.js
+++ src/Components/Dropdown/Dropdown.js
@@ -1,25 +1,44 @@
-import React from 'react';
-import './Dropdown.css';
+import React, { useState } from 'react';
 import { Menu, Dropdown, Button } from 'antd';
 import { DownOutlined } from '@ant-design/icons';
-
-export default function DropdownButton(props) {
+import './Dropdown.css';
+export default function DropdownButton({
+  elementArray,
+  activitiesArray,
+  handleSelect,
+  elem,
+  dropdownTitle,
+}) {
+  const [elementChecked, setElementChecked] = useState(dropdownTitle);
+  const handleClick = (element) => {
+    console.log(element.item.props.value);
+    let value = element.item.props.value;
+    setElementChecked(value);
+    activitiesArray.forEach((act) => {
+      if (act[elem] == value) {
+        handleSelect(act);
+      }
+    });
+  };
   const menu = (
-    <Menu>
-      {props.list.map((item) => (
-        <Menu.Item className="items"> {item} </Menu.Item>
+    <Menu className="itemsContainer">
+      {elementArray.map((item, index) => (
+        <Menu.Item
+          className="items"
+          value={item.name}
+          key={index}
+          onClick={(element) => handleClick(element)}
+        >
+          {item.name}
+        </Menu.Item>
       ))}
     </Menu>
   );
   return (
     <div>
-      <Dropdown overlay={menu} placement="bottomCenter" arrow>
-        <Button
-          className="dropButton"
-          type={props.buttonType}
-          shape={props.buttonshape}
-        >
-          {props.buttonText} <DownOutlined />
+      <Dropdown overlay={menu}>
+        <Button className="dropButton" shape="round">
+          {elementChecked} <DownOutlined />
         </Button>
       </Dropdown>
     </div>
