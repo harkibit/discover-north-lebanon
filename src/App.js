f2093da0a1efec3a2521d988fac9f64a1eb9dad0
charbel-t <charbel2000.52@gmail.com>
2021-02-25 16:48:02 +0200

    Form added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import 'antd/dist/antd.css';
+import Feedback from './Components/FeedbackForm/FeedbackForm';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Feedback />
+    </div>
+  );
 }
 
 export default App;
--- src/Components/FeedbackForm/FeedbackForm.js
+++ src/Components/FeedbackForm/FeedbackForm.js
@@ -1,45 +1,57 @@
 import React from 'react';
 import './FeedbackForm.css';
-import { Layout, Row, Col, Typography, Input, Button } from 'antd';
+import { Layout, Row, Col, Typography, Input, Button, Form } from 'antd';
 import { SendOutlined, UserOutlined } from '@ant-design/icons';
 const { Title } = Typography;
 
 function FeedbackFormComponent({ onClickFct }) {
   return (
     <div>
-      <Layout className="layoutHeight">
-        <Row className="firstRowMargin">
-          <Col span={8} offset={3}>
-            <Title level={2}>Your Opinion Always Matters</Title>
-            <Title level={5}>
-              After visiting our site, you may have some advises. We would like
-              to hear them from you.{' '}
-            </Title>
-          </Col>
+      <Form>
+        <Layout className="layoutHeight">
+          <Row className="firstRowMargin">
+            <Col span={8} offset={3}>
+              <Title level={2}>Your Opinion Always Matters</Title>
+              <Title level={5}>
+                After visiting our site, you may have some advises. We would
+                like to hear them from you.
+              </Title>
+            </Col>
 
-          <hr className="hr-vertical" />
+            <hr className="hr-vertical" />
 
-          <Col span={8} offset={-2} className="colMarginTop">
-            <Input
-              placeholder="  Your Email..."
-              prefix={<UserOutlined className="userIcon" />}
-              required
-            />
-          </Col>
-        </Row>
-        <Row justify="end">
-          <Col span={8} className="colMargin2">
-            <Input placeholder="Express yourself" required />
-          </Col>
-        </Row>
-        <Row>
-          <Col span={6} offset={17}>
-            <Button className="sendButton" type="primary" onClick={onClickFct}>
-              Send <SendOutlined />
-            </Button>
-          </Col>
-        </Row>
-      </Layout>
+            <Col span={8} offset={-2} className="colMarginTop">
+              <Form.Item>
+                <Input
+                  placeholder="  Your Email..."
+                  prefix={<UserOutlined className="userIcon" />}
+                  required
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row justify="end">
+            <Col span={8} className="colMargin2">
+              <Form.Item>
+                <Input placeholder="Express yourself" required />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={6} offset={17}>
+              <Form.Item>
+                <Button
+                  className="sendButton"
+                  type="primary"
+                  onClick={onClickFct}
+                >
+                  Send <SendOutlined />
+                </Button>
+              </Form.Item>
+            </Col>
+          </Row>
+        </Layout>
+      </Form>
     </div>
   );
 }
