2de8acf6d28d2518cc3f354a79ce052cb55abc30
hiba <hibaabdelkarim2000@gmail.com>
2021-02-26 05:42:55 +0200

    removed app file content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import 'antd/dist/antd.css';
 import React, { Suspense } from 'react';
-import NoFavoriteItem from './Components/NoFavoriteItem/NoFavoriteItem';
 
 function App() {
   return (
     <div className="App">
       <Suspense fallback="loading"></Suspense>
-      <NoFavoriteItem />
     </div>
   );
 }
