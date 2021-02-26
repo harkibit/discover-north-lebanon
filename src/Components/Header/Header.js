73dde55b677e3ac52341f3204536083a416a881d
Bakhos Ibrahim-Rachwan <bakhos.rachwan@gmail.com>
2021-02-26 15:10:27 +0200

    Feat: header component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,6 @@
 import './Header.css';
 import Search from '../Search/Search';
-import videos from './headerGif.mp4';
+import videos from './headerGiffy.mp4';
 import { gems } from '../../MockData/data';
 
 const Header = () => {
@@ -8,7 +8,7 @@ const Header = () => {
     <div className="header">
       <video src={videos} autoPlay />
       <div className="titleContainer">
-        <h1 className="title">Discover North Lebanon</h1>
+        <h1>Discover North Lebanon</h1>
         <h2>Every City has its own stuff, get to know them more with us</h2>
       </div>
       <Search citiesArray={gems} activitiesArray={gems} />
