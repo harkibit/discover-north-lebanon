3fc068af3ab26be92d287ba9b4cb111e5d28ba01
charbel-t <charbel2000.52@gmail.com>
2021-02-27 10:24:42 +0200

    feat: routing setup + navbar component links added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import 'antd/dist/antd.css';
 import React, { Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Suspense fallback="loading"></Suspense>
+      <Router>
+        <Suspense fallback="loading">
+          <Switch>
+            <Route exact path="/">
+              {/* Home page */}
+            </Route>
+            <Route exact path="/activities">
+              {/* Activities page */}
+            </Route>
+            <Route exact path="/cities">
+              {/* Cities page */}
+            </Route>
+            <Route exact path="/tour-guide">
+              {/* TourGuide page */}
+            </Route>
+            <Route exact path="/user-profile">
+              {/* UserProfile page */}
+            </Route>
+            <Route exact path="/about-us">
+              {/* AboutUs page */}
+            </Route>
+            <Route exact path="*">
+              <h1>You just made a BIG FAT ERROR! Apocalypse!</h1>
+              {/* Error page (To be created maybe) */}
+            </Route>
+          </Switch>
+        </Suspense>
+      </Router>
     </div>
   );
 }
