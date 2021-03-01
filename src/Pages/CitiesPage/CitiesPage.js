c64356d629a2b328a11452c978a8b0ca7374a450
hiba <hibaabdelkarim2000@gmail.com>
2021-03-01 12:01:40 +0200

    fix : name fix

--- src/Pages/CitiesPage/CitiesPage.js
+++ src/Pages/CitiesPage/CitiesPage.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import CITIES from '../../MockData/cities.json';
 import CityCardItem from '../../Components/CityCardItem/CityCardItem.js';
 import './CitiesPage.css';
 
-export default function CitiesPage() {
+export default function CitiesPage({ data }) {
   return (
     <div className="cityPage">
       <div className="cityList">
-        {CITIES.map((city, i) => {
-          return (
-            <div key={i} className="cityLs">
-              <CityCardItem
-                imgsrc={city.image}
-                name={city.name}
-                info={city.description}
-                activityNumber={city.activity.length}
-              />
-            </div>
-          );
+        {data.map((city, i) => {
+          <div key={i} className="cityLs">
+            <CityCardItem
+              imgsrc={city.image}
+              name={city.name}
+              info={city.description}
+              activityNumber={city.activity.length}
+            />
+          </div>;
         })}
       </div>
     </div>
