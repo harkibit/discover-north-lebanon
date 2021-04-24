bab8bbd8f22818b039375610603dcfe4edcc990a
Bakhos Ibrahim-Rachwan <bakhos.rachwan@gmail.com>
2021-04-24 20:56:01 +0300

    Style: fix responsive

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { Layout, Menu, Dropdown, Affix } from 'antd';
-import { GlobalOutlined } from '@ant-design/icons';
+import { Layout, Menu, Dropdown, Affix, Button, Drawer } from 'antd';
+import { GlobalOutlined, MenuFoldOutlined } from '@ant-design/icons';
 import './Navbar.css';
 import ButtonComponont from '../Button/Button';
 import { useTranslation } from 'react-i18next';
@@ -44,45 +44,50 @@ export default function Navbar() {
       })}
     </Menu>
   );
+  const Middle = ({ modes }) => {
+    return (
+      <Menu className="headerStyleCenter" mode={modes}>
+        <Menu.Item key="1">
+          <Link to="/">
+            <span className="navbar-items">{t('navbar.home')}</span>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="2">
+          <Link to="/activities">
+            <span className="navbar-items">{t('navbar.activities')}</span>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="3">
+          <Link to="/cities">
+            <span className="navbar-items">{t('navbar.cities')} </span>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="4">
+          <Link to="/tour-guide">
+            <span className="navbar-items">{t('navbar.tourGuide')} </span>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="5">
+          <Link to="/about-us">
+            <span className="navbar-items">{t('navbar.about')} </span>
+          </Link>
+        </Menu.Item>
+      </Menu>
+    );
+  };
   const [top] = useState(0);
+  const [visible, setVisible] = useState(false);
+
   return (
     <Affix offsetTop={top}>
-      <div>
-        <Layout id="navbar-header-style">
-          <div className="languages-toggle-lr">
-            <img
-              className="logo"
-              alt="discover the north Logo"
-              src={logo}
-            ></img>
-          </div>
-          <Menu className="headerStyleCenter" mode="horizontal">
-            <Menu.Item key="1">
-              <Link to="/">
-                <span className="navbar-items">{t('navbar.home')}</span>
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Link to="/activities">
-                <span className="navbar-items">{t('navbar.activities')}</span>
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="3">
-              <Link to="/cities">
-                <span className="navbar-items">{t('navbar.cities')} </span>
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="4">
-              <Link to="/tour-guide">
-                <span className="navbar-items">{t('navbar.tourGuide')} </span>
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="5">
-              <Link to="/about-us">
-                <span className="navbar-items">{t('navbar.about')} </span>
-              </Link>
-            </Menu.Item>
-          </Menu>
+      <Layout id="navbar-header-style">
+        <div className="languages-toggle-lr">
+          <img className="logo" alt="discover the north Logo" src={logo}></img>
+        </div>
+        <div className="middle-nav">
+          <Middle modes="horizontal" />
+        </div>
+        <div className="left-part">
           <div className="languages-toggle-lr">
             <Dropdown.Button
               id="langButton"
@@ -90,10 +95,28 @@ export default function Navbar() {
               placement="bottomCenter"
               icon={<GlobalOutlined />}
             />
-            <ButtonComponont text={t('navbar.signInButton')} type="primary" />
+            <div className="sign-in-btn">
+              <ButtonComponont text={t('navbar.signInButton')} type="primary" />
+            </div>
           </div>
-        </Layout>
-      </div>
+          <Button
+            className="barsMenu"
+            type="none"
+            onClick={() => setVisible(!visible)}
+          >
+            <MenuFoldOutlined />
+          </Button>
+          <Drawer
+            placement="right"
+            closable={false}
+            onClose={() => setVisible(!visible)}
+            visible={visible}
+          >
+            <Middle modes="vertical" />
+            <ButtonComponont text={t('navbar.signInButton')} type="primary" />
+          </Drawer>
+        </div>
+      </Layout>
     </Affix>
   );
 }
