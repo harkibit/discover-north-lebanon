b477eba901881b07724a3321a848854a046e3684
Bakhos Ibrahim-Rachwan <bakhos.rachwan@gmail.com>
2021-04-25 15:01:49 +0300

    Style: fix responsive

--- src/Components/TourGuideCard/TourGuideCard.js
+++ src/Components/TourGuideCard/TourGuideCard.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './tourGuideCard.css';
-import { Card, Row, Col, Tooltip, Tag, Skeleton } from 'antd';
+import { Card, Tooltip, Tag, Skeleton } from 'antd';
 import { EllipsisOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 import TourGuideModal from '../TourGuideModal/TourGuideModal';
@@ -12,30 +12,27 @@ function TourGuideCard(tourGuide) {
   const imgstyle = {
     borderRadius: '10px',
   };
+
   return (
-    <Card
-      className="tour-guide-card"
-      cover={
-        <img
-          className="imgProp"
-          style={imgstyle}
-          src={tourGuide.imgsrc}
-          alt="Guider"
-        />
-      }
-    >
-      <Row className="rowheight">
-        <Col span={20}>
-          <Row>
-            <h1>{tourGuide.name}</h1>
-          </Row>
+    <Card className="guide-card" bodyStyle={{ padding: '0' }}>
+      <div className="tour-guide-card1">
+        <div className="guide-pic">
+          <img
+            className="imgProp"
+            style={imgstyle}
+            src={tourGuide.imgsrc}
+            alt="Guider"
+          />
+        </div>
+        <div className="info-section">
+          <h1>{tourGuide.name}</h1>
           <div>
             Speciality : &nbsp;
             {tourGuide.tags &&
               tourGuide.tags.map((tag) => <Tag color="blue">{tag}</Tag>)}
           </div>
 
-          <Row justify="center" className="description">
+          <div justify="center" className="description">
             {!tourGuide.description ? (
               <div className="skeleton-container">
                 <Skeleton.Input active size="small" className="skeleton" />
@@ -53,10 +50,10 @@ function TourGuideCard(tourGuide) {
                 &nbsp;<span className="tour-see-more"> See more ..</span>
               </span>
             )}
-          </Row>
-        </Col>
-        <Col span={4} className="col-align">
-          <Row justify="end">
+          </div>
+        </div>
+        <div className="buttons-section">
+          <div>
             <Tooltip
               placement="top"
               title="See more"
@@ -73,8 +70,8 @@ function TourGuideCard(tourGuide) {
                 handleCancel={handleCancel}
               />
             </Tooltip>
-          </Row>
-          <Row justify="end">
+          </div>
+          <div>
             <Link to={`/hire-me/${tourGuide.name}`}>
               <button
                 className="hire-me-card-btn"
@@ -84,10 +81,86 @@ function TourGuideCard(tourGuide) {
                 Hire me
               </button>
             </Link>
-          </Row>
-        </Col>
-      </Row>
+          </div>
+        </div>
+      </div>
     </Card>
+
+    // <Card
+    //   className="tour-guide-card"
+    //   cover={
+    //     <img
+    //       className="imgProp"
+    //       style={imgstyle}
+    //       src={tourGuide.imgsrc}
+    //       alt="Guider"
+    //     />
+    //   }
+    // >
+    //   <Row className="rowheight">
+    //     <Col span={20}>
+    //       <Row>
+    //         <h1>{tourGuide.name}</h1>
+    //       </Row>
+    //       <div>
+    //         Speciality : &nbsp;
+    //         {tourGuide.tags &&
+    //           tourGuide.tags.map((tag) => <Tag color="blue">{tag}</Tag>)}
+    //       </div>
+
+    //       <Row justify="center" className="description">
+    //         {!tourGuide.description ? (
+    //           <div className="skeleton-container">
+    //             <Skeleton.Input active size="small" className="skeleton" />
+    //             <Skeleton.Input
+    //               active
+    //               size="small"
+    //               className="skeleton-small"
+    //             />
+    //           </div>
+    //         ) : tourGuide.description.split(' ').length < 30 ? (
+    //           tourGuide.description
+    //         ) : (
+    //           <span className="tour-guider-desc">
+    //             {tourGuide.description.split(' ').slice(0, 30).join(' ')}
+    //             &nbsp;<span className="tour-see-more"> See more ..</span>
+    //           </span>
+    //         )}
+    //       </Row>
+    //     </Col>
+    //     <Col span={4} className="col-align">
+    //       <Row justify="end">
+    //         <Tooltip
+    //           placement="top"
+    //           title="See more"
+    //           arrowPointAtCenter
+    //           onClick={() => null}
+    //         >
+    //           <EllipsisOutlined
+    //             className="icon-size"
+    //             onClick={() => setModal(true)}
+    //           />
+    //           <TourGuideModal
+    //             visible={modal}
+    //             guide={tourGuide.guide}
+    //             handleCancel={handleCancel}
+    //           />
+    //         </Tooltip>
+    //       </Row>
+    //       <Row justify="end">
+    //         <Link to={`/hire-me/${tourGuide.name}`}>
+    //           <button
+    //             className="hire-me-card-btn"
+    //             text="Hire me"
+    //             type="primary"
+    //           >
+    //             Hire me
+    //           </button>
+    //         </Link>
+    //       </Row>
+    //     </Col>
+    //   </Row>
+    // </Card>
   );
 }
 export default TourGuideCard;
